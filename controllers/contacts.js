447e33a4fb7edca3f534efaead43f66fb3842f6d
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-09-13 16:57:00 -0700

    completed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,14 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
-const contacts = require("../models/contacts");
+const {Contact} = require("../models/contact");
 
 
 const getAll = async (req, res) => {
-    res.json(await contacts.listContacts());
+    res.json(await Contact.find());
 };
 
 const getById = async (req, res) => {
     const { contactId } = req.params;
-    const response = await contacts.getContactById(contactId);
+    const response = await Contact.findById(contactId);
     if (!response) {
         throw HttpError(404, "Not found");
     }
@@ -16,13 +16,13 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-    const result = await contacts.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
 };
 
 const deleteById = async (req, res) => {
     const { contactId } = req.params;
-    const response = await contacts.removeContact(contactId);
+    const response = await Contact.findByIdAndRemove(contactId);
     if (!response) {
         throw HttpError(404, "Not found");
     }
@@ -33,17 +33,29 @@ const deleteById = async (req, res) => {
 
 const updateById = async (req, res) => {
     const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new:true});
     if (!result) {
         throw HttpError(404, "Not found");
     }
     return res.json(result);
 };
 
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  return res.json(result);
+};
+
 module.exports = {
-    getAll: ctrlWrapper(getAll),
-    getById: ctrlWrapper(getById),
-    add: ctrlWrapper(add),
-    deleteById: ctrlWrapper(deleteById),
-    updateById: ctrlWrapper(updateById),
-}
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  deleteById: ctrlWrapper(deleteById),
+  updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST).then(() => {
+  console.log("Database connection successful");
   app.listen(PORT)
 }).catch(error => {
   console.log(error.message);
